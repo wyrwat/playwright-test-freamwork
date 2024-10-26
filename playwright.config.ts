f8220818949ca38b56ecbcb97ff65d31dd899aa9
Tomasz Wyrwa <tomasz.wyrwa@appfire.com>
2024-10-26 18:18:53 +0200

    init project

---
+++ playwright.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig, devices } from '@playwright/test';
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
+export default defineConfig({
+  testDir: './tests',
+  timeout: 60_000,
+  expect: { timeout: 10_000 },
+  fullyParallel: true,
+  workers: undefined,
+  reporter: 'html',
+  use: {
+    actionTimeout: 0,
+    trace: 'retain-on-failure',
+    video: 'retain-on-failure',
+    screenshot: 'only-on-failure',
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
+});
