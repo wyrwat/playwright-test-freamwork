e8b3f438fb3210220fd379e604c76fb4bc263746
tom <wyrwa.t@gmail.com>
2024-11-04 12:19:09 +0100

    feat add login negative test

--- src/pages/login.page.ts
+++ src/pages/login.page.ts
@@ -1,16 +1,26 @@
 import { BasePage } from './base.page';
-import { Page } from '@playwright/test';
+import { Locator, Page } from '@playwright/test';
 
 export class LoginPage extends BasePage {
   url = '/login/';
+  userEmailInput: Locator;
+  userPasswordInput: Locator;
+  loginButton: Locator;
+  loginError: Locator;
+
   constructor(page: Page) {
     super(page);
+    this.userEmailInput = this.page.getByPlaceholder('Enter User Email');
+    this.userPasswordInput = this.page.getByPlaceholder('Enter Password');
+    this.loginButton = this.page.getByRole('button', { name: 'LogIn' });
+
+    this.loginError = this.page.getByTestId('login-error');
   }
 
   async login(email: string, password: string): Promise<void> {
     await this.page.waitForLoadState();
-    await this.page.getByPlaceholder('Enter User Email').fill(email);
-    await this.page.getByPlaceholder('Enter Password').fill(password);
-    await this.page.getByRole('button', { name: 'LogIn' }).click();
+    await this.userEmailInput.fill(email);
+    await this.userPasswordInput.fill(password);
+    await this.loginButton.click();
   }
 }
--- tests/smoke/login.spec.ts
+++ tests/smoke/login.spec.ts
@@ -26,4 +26,30 @@ test.describe('Verify menu main buttons', () => {
       );
     },
   );
+
+  test(
+    'Reject login with incorrect password',
+    { tag: ['@smoke', '@GAD-R02-02'] },
+    async ({ page }) => {
+      //Arrange
+      const userEmail = testUser1.userEmail;
+      const userPassword = 'incorrectPassword';
+      const loginPage = new LoginPage(page);
+
+      //Act
+      await loginPage.goto();
+      await loginPage.login(userEmail, userPassword);
+
+      //Assert
+      await expect(loginPage.loginError).toContainText(
+        'Invalid username or password',
+      );
+      // const welcomePage = new WelcomePage(page);
+      // // const title = await welcomePage.title();
+      // // expect(title).toContain('Welcome');
+      // // await expect(welcomePage.welcome).toContainText(
+      // //   'Hi Moses.Armstrong@Feest.ca!',
+      // );
+    },
+  );
 });
