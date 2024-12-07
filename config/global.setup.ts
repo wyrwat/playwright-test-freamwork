bb2e1310f930a33cd74f58463654e04a2d987223
wyrwat <wyrwa.t@gmail.com>
2024-12-07 14:32:00 +0100

    rgergerg

--- config/global.setup.ts
+++ config/global.setup.ts
@@ -5,6 +5,9 @@ async function globalSetup(): Promise<void> {
   if (fs.existsSync(STORAGE_STATE)) {
     fs.unlinkSync(STORAGE_STATE);
   }
+
+  // console.log('⚠️ Global setup');
 }
 
 export default globalSetup;
+
