49c4d52d8cc9174339e6b8ccb14d6c34834014cc
wyrwat <wyrwa.t@gmail.com>
2025-01-10 07:00:26 +0100

    chore: refactor api test

--- tests/api/articles.smoke.spec.ts
+++ tests/api/articles.smoke.spec.ts
@@ -1,15 +1,19 @@
 import { expect, test } from '@_src/fixtures/merge.fixture';
 
 test.describe('Verify articles API endpoint', () => {
-  test('GET articles return status code 200', async ({ request }) => {
-    // Arrange
-    const expectedStatusCode = 200;
-    const articleUrl = '/api/articles';
+  test(
+    'GET articles return status code 200',
+    { tag: ['@GAD-R08-01', '@api'] },
+    async ({ request }) => {
+      // Arrange
+      const expectedStatusCode = 200;
+      const articleUrl = '/api/articles';
 
-    // Act
-    const response = await request.get(articleUrl);
+      // Act
+      const response = await request.get(articleUrl);
 
-    // Assert
-    expect(response.status()).toBe(expectedStatusCode);
-  });
+      // Assert
+      expect(response.status()).toBe(expectedStatusCode);
+    },
+  );
 });
