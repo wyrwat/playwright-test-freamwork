ad55f6fc49c3089927fdeab305a8e747d9bddb19
wyrwat <wyrwa.t@gmail.com>
2025-01-11 14:09:15 +0100

    test: api smoeke test article extended test

--- tests/api/articles.smoke.spec.ts
+++ tests/api/articles.smoke.spec.ts
@@ -16,4 +16,23 @@ test.describe('Verify articles API endpoint', () => {
       expect(response.status()).toBe(expectedStatusCode);
     },
   );
+
+  test(
+    'Get acrticle should return at least one article',
+    { tag: ['@GAD-R08-01', '@api'] },
+    async ({ request }) => {
+      // Arrange
+      const expectedMinArticleCount = 1;
+      const articleUrl = '/api/articles';
+
+      // Act
+      const response = await request.get(articleUrl);
+      const responseJson = await response.json();
+
+      // Assert
+      expect(responseJson.length).toBeGreaterThanOrEqual(
+        expectedMinArticleCount,
+      );
+    },
+  );
 });
