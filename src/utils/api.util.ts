8cf5e35cbe26c269d9f61142fe943bd65bdd0f72
wyrwat <wyrwa.t@gmail.com>
2025-01-14 17:34:45 +0100

    chore: create createArticlePayload

--- src/utils/api.util.ts
+++ src/utils/api.util.ts
@@ -1,8 +1,16 @@
+import createRandomNewArticle from '@_src/factories/article.factory';
 import { testUser1 } from '@_src/test-data/user.data';
 import { APIRequestContext } from '@playwright/test';
 
 interface Headers {
   [key: string]: string;
+}
+
+interface ArticlePayload {
+  title: string;
+  body: string;
+  date: Date;
+  image: string;
 }
 
 export async function getAuthHeader(
@@ -20,4 +28,18 @@ export async function getAuthHeader(
   return {
     Authorization: `Bearer ${responseLoginJson.access_token}`,
   };
+}
+
+export function createArticlePayload(): ArticlePayload {
+  const currentDate = new Date();
+  const randomArticledata = createRandomNewArticle();
+  const articleData = {
+    title: randomArticledata.title,
+    body: randomArticledata.body,
+    date: currentDate,
+    image:
+      '.\\data\\images\\256\\tester-app_9f26eff6-2390-4460-8829-81a9cbe21751.jpg',
+  };
+
+  return articleData;
 }
